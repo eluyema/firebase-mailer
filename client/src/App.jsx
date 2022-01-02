bfba908d339fe97ed0019b989d534ce3bbad8fb3
eluyemaStudy <70893846+eluyema@users.noreply.github.com>
2022-01-02 02:10:07 +0200

    add form and some validation

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,24 +1,76 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
+import { useForm } from 'react-hook-form';
+import './App.scss';
+import errorIcon from './images/error.svg';
 function App() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors, isValid },
+  } = useForm({ mode: 'onBlur' });
+  const onSubmit = (data) => {
+    alert(JSON.stringify(data));
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload!
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="form-wrapper">
+      <form onSubmit={handleSubmit(onSubmit)} className="form-mailer">
+        <label className="input-field">
+          <p className="input-name">Your name</p>
+          <input
+            {...register('name', {
+              required: { value: true, message: 'Поле должно быть заполненно' },
+            })}
+            className="text-input"
+          />
+          <p className="input-error">
+            {errors?.name && (
+              <>
+                <img src={errorIcon} alt="" className="error-icon" />
+                {errors?.name.message}
+              </>
+            )}
+          </p>
+        </label>
+        <label className="input-field">
+          <p className="input-name">Recipient email</p>
+          <input
+            {...register('email', {
+              required: { value: true, message: 'Поле должно быть заполненно' },
+              pattern: {
+                value: /^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$/,
+                message: 'Вписан неккоректный email',
+              },
+            })}
+            className="text-input"
+          />
+          <p className="input-error">
+            {errors?.email && (
+              <>
+                <img src={errorIcon} alt="" className="error-icon" />
+                {errors?.email.message}
+              </>
+            )}
+          </p>
+        </label>
+        <label className="input-field">
+          <p className="input-name">Your message</p>
+          <textarea
+            {...register('text', {
+              required: { value: true, message: 'Поле должно быть заполненно' },
+            })}
+            className="textarea-input"
+          />
+          <p className="input-error">
+            {errors?.text && (
+              <>
+                <img src={errorIcon} alt="" className="error-icon" />
+                {errors?.text.message}
+              </>
+            )}
+          </p>
+        </label>
+        <input type="submit" disabled={!isValid} className="submit-button" />
+      </form>
     </div>
   );
 }
