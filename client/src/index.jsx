2599fab7fbb27e3d25965d005f08cd8c6d8ca61b
eluyemaStudy <70893846+eluyema@users.noreply.github.com>
2022-01-03 02:38:57 +0200

    fix absolute links

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import '/web-lab-2/client/index.scss';
-import '/web-lab-2/client/styles/reset.scss';
-import App from '/web-lab-2/client/App';
+import '/web-lab-2/client/src/index.scss';
+import '/web-lab-2/client/src/styles/reset.scss';
+import App from '/web-lab-2/client/src/App';
 
 ReactDOM.render(
   <React.StrictMode>
