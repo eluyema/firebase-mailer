4b56a421cd143dab2a7733bfb7402d15bbfcedfa
eluyemaStudy <70893846+eluyema@users.noreply.github.com>
2022-01-14 19:22:13 +0200

    fix ratelimit

--- functions/ratelimit/ratelimit.js
+++ functions/ratelimit/ratelimit.js
@@ -4,13 +4,10 @@ function ratelimit(request, ipCounter) {
   const userIP =
     (request.headers['x-forwarded-for'] || '').split(',')[0] ||
     request.connection.remoteAddress;
-  let rateNum = 1;
-  if (ipCounter.has(userIP)) {
-    rateNum = ipCounter.get(userIP) + 1;
-    if (rateNum <= 5) ipCounter.set(userIP, rateNum);
-  } else {
-    ipCounter.set(userIP, rateNum);
-  }
+  let rateNum = ipCounter.get(userIP) || 1;
+
+  ipCounter.set(userIP, rateNum++);
+
   return rateNum <= 5;
 }
 
